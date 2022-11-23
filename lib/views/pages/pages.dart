84b15022440573cad5046098fc8a759eb477e42f
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2022-11-23 09:41:38 +0700

    firstinit

---
+++ lib/views/pages/pages.dart
@@ -0,0 +1,7 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:firebaseproject/services/services.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+part "register.dart";
+part "login.dart";
